8930e289d19b271d24a0aa981057d560e27bffbf
basemmo <basem457@revature.net>
2022-04-21 20:16:27 -0400

    Random data for state, country, dates

--- insurance/src/main/scala/example/generator.scala
+++ insurance/src/main/scala/example/generator.scala
@@ -5,16 +5,19 @@ import java.io.File
 import scala.io.Source
 import java.util.Properties
 import scala.util.Random
+import java.util.Calendar;
+import java.time.LocalDateTime
+import javax.print.attribute.DateTimeSyntax
 
 
 object genData {
-    val random = new Random()
 
     def main(args: Array[String]):Unit = {
-        println(data())
+        println(randomDate())
     }
 
     val random = new Random()
+
 
     def name(): String = {
         val filePath = "src/main/scala/example/names.txt"
@@ -30,6 +33,39 @@ object genData {
     }
     def data(): Any = {
         val cvsFeilds = Array("id", "name", "age")
+    }
+
+
+    def country(): String = {
+        val country = "United States of America"
+        return country
+    }
+
+    def state(): String = {
+        val filePath = "src/main/scala/example/states.txt"
+        val file = new File(filePath)
+        val stateList = Source.fromFile(file).getLines().toList
+        val state = stateList(random.nextInt(stateList.length))
+        return state
+    }
+
+    // def randomDate(): String = {
+    //     val day = new scala.util.Random.nextInt(30).toString
+    //     val month = new scala.util.Random.nextInt(12).toString
+    //     val year = new scala.util.Random.nextInt(2022).toString
+    //     print(day)
+    // }
+
+
+    def randomDate(): String = {
+        val yearAdd = 201
+        val month = scala.util.Random
+        val day = scala.util.Random
+        val year = scala.util.Random
+        // println((month.nextInt(12).toString), (day.nextInt(30).toString))
+        println(month.nextInt(12).toString)
+        println(day.nextInt(30).toString)
+        yearAdd.toString + year.nextInt(9).toString
     }
 
 }

e2f10da39c6d0b8582eb93c753202f82d7c10aad
HelloTeamPlz <mrtommynell@gmail.com>
2022-04-25 15:31:30 -0400

    created from csvfile to pull data into a dataframe

--- insurance/src/main/scala/example/load_analyze.scala
+++ insurance/src/main/scala/example/load_analyze.scala
@@ -5,14 +5,16 @@ package example
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
 
 object Analyze {
 
   val path = "hdfs://sandbox-hdp.hortonworks.com:8020/user/maria_dev/"
 
   def copyFromLocal(): Unit = {
-    val src = "file:///home/maria_dev/files2.txt"
-    val target = path + "files2.txt"
+    val src = "file:///home/maria_dev/insurance.csv"
+    val target = path + "insurance.csv"
     println(s"Copying local file $src to $target ...")
     
     val conf = new Configuration()
@@ -24,4 +26,23 @@ object Analyze {
     fs.copyFromLocalFile(false, localpath, hdfspath)
     println(s"Done copying local file $src to $target ...")
   }
+
+def fromCSVFile(): Unit = {
+    val spark: SparkSession = SparkSession
+        .builder()
+        .master("local[3]")
+        .appName("Synergy")
+        .getOrCreate()
+        spark.sparkContext.setLogLevel("ERROR")
+
+    val sc = spark.sparkContext
+    val df = spark.read
+      .option("header", true)
+      .option("inferSchema", true)
+      .csv("file:///home/maria_dev/insurance.csv")
+    
+    //df.show()
+    //df.createOrReplaceTempView("insuranceTable") 
+    //spark.newSession().sql("SELECT * FROM insuranceTable").show()
+    }
 }
